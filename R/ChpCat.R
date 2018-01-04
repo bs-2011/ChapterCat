860a46c3335222f9fee3f8987b2e90542c674de3
bs-2011 <balveers871@gmail.com>
2018-01-04 18:22:32 +0530

    changes

--- R/ChpCat.R
+++ R/ChpCat.R
@@ -1,8 +1,3 @@
-pca = readRDS(file = "/datadrive/model/pca.rds")
-course1 = readRDS(file = "./man/course1.rds")
-course1_pca = readRDS(file = "./man/course1_pca.rds")
-library(caret)
-library(e1071)
 #' A Predict_Cluster Function
 #'
 #' This function allows you to predict the cluster group in which point lies
@@ -15,8 +10,7 @@ library(e1071)
 
 # predicting the cluster
 predict_cl <- function(input){
-#  require(caret)
-#  require(e1071)
+
   #input can either be csv file or data
   newdata <- if(is.character(input) && file.exists(input)){
     read.csv(input)
@@ -30,28 +24,26 @@ predict_cl <- function(input){
   stopifnot("Opportunity.SubSkills." %in% names(newdata))
   stopifnot("Correct.First.Attempt" %in% names(newdata))
 
- summary = sapply(course1[,c(4:13)], function(cl) list(means=mean(cl,na.rm=TRUE), sds=sd(cl,na.rm=TRUE)))
- summary = as.data.frame(summary)
+  course1 = read.csv(file = "/datadrive/dataset/bclust_sample_v2.csv",header = T)
+  course1_pca = read.csv(file = "/datadrive/dataset/course1pca_Chaptercat.csv",header = T)
 
- # Fitting Hierarchical Clustering to the dataset
- hc = hclust(d = dist(course1_pca[,c(4,5)], method = 'euclidean'), method = 'ward.D')
- y_hc = cutree(hc, 3)
+  summary = sapply(course1[,c(4:13)], function(cl) list(means=mean(cl,na.rm=TRUE), sds=sd(cl,na.rm=TRUE)))
+  summary = as.data.frame(summary)
 
- # function to find centroid in cluster i
- clust.centroid = function(i, dat, clusters) {
-   ind = (clusters == i)
-   colMeans(dat[ind,])
- }
+  # Fitting Hierarchical Clustering to the dataset
+  hc = hclust(d = dist(course1_pca[,c(4,5)], method = 'euclidean'), method = 'ward.D')
+  y_hc = cutree(hc, 3)
 
- centroid = sapply(unique(y_hc), clust.centroid, course1[,c(4:13)], y_hc)
- centroid = as.data.frame(t(centroid))
+  # function to find centroid in cluster i
+  clust.centroid = function(i, dat, clusters) {
+    ind = (clusters == i)
+    colMeans(dat[ind,])
+  }
 
- centroid_pca = sapply(unique(y_hc), clust.centroid, course1_pca[,c(4:5)], y_hc)
- centroid_pca = as.data.frame(t(centroid_pca))
+  centroid = sapply(unique(y_hc), clust.centroid, course1[,c(4:13)], y_hc)
+  centroid = as.data.frame(t(centroid))
 
- newdata_pca = predict(pca, newdata)
-
-  points = rbind(centroid_pca,newdata_pca[,c("PC1","PC2")])
+  points = rbind(centroid,newdata[,c(4:13)])
   dist_matrix = dist(points)
   dist_matrix = as.matrix(dist_matrix)
   dist_matrix = as.data.frame(dist_matrix)
